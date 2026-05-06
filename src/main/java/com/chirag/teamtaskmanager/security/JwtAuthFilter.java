7abe844c35922ce1cea7fc37890ec7c8f4cb5887
chirag31daorha <chirag2001daorha@gmail.com>
2026-05-06 22:25:20 +0530

    updated jwtauthfilter

--- src/main/java/com/chirag/teamtaskmanager/security/JwtAuthFilter.java
+++ src/main/java/com/chirag/teamtaskmanager/security/JwtAuthFilter.java
@@ -26,7 +26,16 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+                                    FilterChain filterChain)
+                                    throws ServletException, IOException {
+
+        String path = request.getServletPath();
+
+        // ✅ ADD THIS
+        if (path.startsWith("/api/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String authHeader = request.getHeader("Authorization");
 
