66a718762e253c9876954f5825d5d04e7b63b45b
chirag31daorha <chirag2001daorha@gmail.com>
2026-05-06 20:46:27 +0530

    updated cors configuration

--- src/main/java/com/chirag/teamtaskmanager/config/SecurityConfig.java
+++ src/main/java/com/chirag/teamtaskmanager/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://team-task-manager-frontend-dun.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
